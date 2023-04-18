2a96f0c402fdac2c8bc4227ba284be2671ed6bc5
Luis Manjarrez carbajal <darenhein@Hotmail.com>
2023-04-17 20:52:08 -0600

    v1

---
+++ src/Ventana.java
@@ -0,0 +1,66 @@
+import javax.swing.JFrame;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*; 
+
+public class Ventana extends JFrame{
+
+    private int ancho = 400; 
+    private int largo = 400;
+    private JPanel p; 
+    private JLabel e; 
+    private JButton b,b1; 
+
+    public Ventana(){
+        this.setSize(ancho, largo);
+        this.setLocationRelativeTo(null);
+        this.setTitle("Arduino");
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setResizable(false);
+        paneles(); 
+    }
+    public void paneles(){
+        p = new JPanel(); 
+        p.setLayout(null);
+        this.getContentPane().add(p);
+        etis(); 
+        botones(); 
+    }
+    public void etis(){
+
+      
+
+    }
+    public void botones(){
+        b = new JButton("Encender"); 
+        b.setBounds(150, 150, 120, 20);
+        b.addActionListener(new ActionListener(){
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            
+            }
+            
+        });
+        p.add(b);
+        
+        b1 = new JButton(); 
+        b1.setBounds(150, 250, 120, 20);
+        b1.setText("apagar"); 
+        b1.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                
+            }
+            
+        });
+        p.add(b1); 
+
+      
+
+    }
+    
+}
